9ce5068248ba5c4332009383b85cc789ac8d47b7
Mustapha Momoh <mmomoh@uwaterloo.ca>
2025-06-09 13:31:56 -0400

    ingest pipeline added

--- search.py
+++ search.py
@@ -12,51 +12,67 @@ load_dotenv()
 class Search:
     def __init__(self):
         self.ops = OpenSearch(
-            hosts = [{'host': 'localhost', 'port': 19200}],
-            http_compress = True, # enables gzip compression for request bodies
-            http_auth = (os.getenv("OPENSEARCH_ADMIN_USER"), os.getenv("OPENSEARCH_INITIAL_ADMIN_PASSWORD")),
-            use_ssl = True,
-            verify_certs = False,
-            ssl_assert_hostname = False,
-            ssl_show_warn = False,
-
+            hosts=[{"host": "localhost", "port": 19200}],
+            http_compress=True,  # enables gzip compression for request bodies
+            http_auth=(
+                os.getenv("OPENSEARCH_ADMIN_USER"),
+                os.getenv("OPENSEARCH_INITIAL_ADMIN_PASSWORD"),
+            ),
+            use_ssl=True,
+            verify_certs=False,
+            ssl_assert_hostname=False,
+            ssl_show_warn=False,
         )
         client_info = self.ops.info()
-        print('Connected to Opensearch!')
+        print("Connected to Opensearch!")
         pprint(client_info)
 
     def create_index(self):
-        self.ops.indices.delete(index='my_documents', ignore_unavailable=True)
-        self.ops.indices.create(index='my_documents', mappings={
-            'properties': {
-                'content_embedding': {
-                    'type': 'dense_vector',
-                }
-            }
-        })
-
+        self.ops.indices.delete(index="my_documents", ignore_unavailable=True)
+        self.ops.indices.create(
+            index="my_documents",
+            body={
+                "settings": {
+                    "index.knn": True,
+                    "default_pipeline": "embedding-ingest-pipeline-l6mini",
+                },
+                "mappings": {
+                    "properties": {
+                        "content_embedding": {
+                            "type": "knn_vector",
+                            "dimension": 384,
+                            "method": {
+                                "name": "hnsw",
+                                "space_type": "innerproduct",
+                                "engine": "lucene",
+                            },
+                        }
+                    }
+                },
+            },
+        )
 
     def insert_document(self, document):
-        return self.ops.index(index='my_documents', body=document)
-    
+        return self.ops.index(index="my_documents", body=document)
+
     def insert_documents(self, documents):
         operations = []
         for document in documents:
-            operations.append({'index': {'_index': 'my_documents'}})
+            operations.append({"index": {"_index": "my_documents"}})
             operations.append(document)
         return self.ops.bulk(body=operations)
-    
+
     def reindex(self):
         self.create_index()
-        with open('data.json','rt') as f:
+        with open("data.json", "rt") as f:
             documents = json.loads(f.read())
         return self.insert_documents(documents)
-    
+
     def search(self, **query_args):
-        if 'from_' in query_args:
-            query_args['from']=query_args['from_']
-            del query_args['from_']
-        return self.ops.search(index='my_documents', body=json.dumps(query_args))
-    
+        if "from_" in query_args:
+            query_args["from"] = query_args["from_"]
+            del query_args["from_"]
+        return self.ops.search(index="my_documents", body=json.dumps(query_args))
+
     def retrieve_document(self, id):
-        return self.ops.get(index='my_documents', id=id)
+        return self.ops.get(index="my_documents", id=id)
