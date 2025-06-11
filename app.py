3e035b65c323206631747d98b4cdfb3cf04c9545
Mustapha Momoh <mmomoh@uwaterloo.ca>
2025-06-11 12:45:02 -0400

    new

--- app.py
+++ app.py
@@ -5,90 +5,97 @@ from search import Search
 app = Flask(__name__)
 ops = Search()
 
-@app.get('/')
+
+@app.get("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 def extract_filters(query):
-    filters= []
+    filters = []
 
-    category_regex = r'category:([^\s]+)\s*'
+    category_regex = r"category:([^\s]+)\s*"
     matches = re.search(category_regex, query)
     if matches:
-        filters.append({
-            'term': {
-                'category.keyword': {
-                    'value': matches.group(1)
-                }
-            }
-        })
+        filters.append({"term": {"category.keyword": {"value": matches.group(1)}}})
 
-        #remove the category filter from the query
-        query = re.sub(category_regex, '', query).strip()
+        # remove the category filter from the query
+        query = re.sub(category_regex, "", query).strip()
 
-    #year filter
-    year_regex = r'year:([^\s]+)\s*'
+    # year filter
+    year_regex = r"year:([^\s]+)\s*"
     matches = re.search(year_regex, query)
     if matches:
-        filters.append({
-            'range': {
-                'updated_at': {
-                    'gte': f'{matches.group(1)}||/y',
-                    'lte': f'{matches.group(1)}||/y',
-                }
-            },
-        })
-        #remove the year filter from the query
-        query = re.sub(year_regex, '', query).strip()
+        filters.append(
+            {
+                "range": {
+                    "updated_at": {
+                        "gte": f"{matches.group(1)}||/y",
+                        "lte": f"{matches.group(1)}||/y",
+                    }
+                },
+            }
+        )
+        # remove the year filter from the query
+        query = re.sub(year_regex, "", query).strip()
 
-    return {'filter': filters}, query
+    return {"filter": filters}, query
 
-@app.post('/')
+
+@app.post("/")
 def handle_search():
-    query = request.form.get('query', '')
+    query = request.form.get("query", "")
     filters, parsed_query = extract_filters(query)
-    from_ = request.form.get('from_', type=int, default=0)
+    from_ = request.form.get("from_", type=int, default=0)
 
     if parsed_query:
-        search_query = {
-            "must": {
-                "multi_match": {
-                    "query": parsed_query,
-                    "fields": ["name", "summary", "content"],
-                }
+        lex_query = {
+            "bool": {
+                "must": [
+                    {
+                        "multi_match": {
+                            "query": parsed_query,
+                            "fields": ["name", "summary", "content"],
+                        }
+                    }
+                ],
+                **filters,
             }
         }
-    else:
-        search_query = {"must": {"match_all": {}}}
-
-    # combine the filters and search query with a bool
-    bool_query = {"bool": {**search_query, **filters}}
-
-    # neural query setup with filters
-    model_id = ops.get_model_id("huggingface/sentence-transformers/all-MiniLM-L6-v2")
-    neural_query = {
-        "bool": {
-            "must": {
-                "neural": {
-                    "summary_embedding": {
-                        "query_text": parsed_query,
-                        "model_id": model_id,
-                        "k": 10,  # number of results to return
+        # neural query setup with filters
+        model_id = ops.get_model_id(
+            "huggingface/sentence-transformers/all-MiniLM-L6-v2"
+        )
+        neural_query = {
+            "bool": {
+                "must": [
+                    {
+                        "neural": {
+                            "summary_embedding": {
+                                "query_text": parsed_query,
+                                "model_id": model_id,
+                                "k": 10,  # number of results to return
+                            }
+                        }
                     }
-                }
-            },
-            **filters,
+                ],
+                **filters,
+            }
         }
-    }
+        search_query = {**lex_query, **neural_query}
+    else:
+        search_query = {"bool": {"must": [{"match_all": {}}]}}
+
+    # # combine the filters and search query with a bool
+    # bool_query = {"bool": {**search_query, **filters}}
 
     # combine both bool queries with a hybrid query
     hybrid_query = {
         "hybrid": {
             "queries": [
-                bool_query,
-                neural_query,
+                search_query,
             ],
-            "pagination_depth": 50,  #needed for hybrid queries. It specifies the maximum number of search results to retrieve from each shard for every subquery.
+            "pagination_depth": 50,  # needed for hybrid queries. It specifies the maximum number of search results to retrieve from each shard for every subquery.
         }
     }
 
@@ -108,7 +115,7 @@ def handle_search():
                 },
             },
         },
-		size=5,
+        size=5,
         from_=from_,
     )
 
@@ -126,24 +133,28 @@ def handle_search():
     }
 
     return render_template(
-        'index.html',
-        results=results['hits']['hits'],
+        "index.html",
+        results=results["hits"]["hits"],
         query=query,
         from_=from_,
-        total=results['hits']['total']['value'],
-        aggs=aggs
+        total=results["hits"]["total"]["value"],
+        aggs=aggs,
     )
 
-@app.get('/document/<id>')
+
+@app.get("/document/<id>")
 def get_document(id):
     document = ops.retrieve_document(id)
-    title = document['_source']['name']
-    paragraphs = document['_source']['content'].split('\n')
-    return render_template('document.html', title=title, paragraphs=paragraphs)
+    title = document["_source"]["name"]
+    paragraphs = document["_source"]["content"].split("\n")
+    return render_template("document.html", title=title, paragraphs=paragraphs)
+
 
 @app.cli.command()
 def reindex():
     """Regenerate the Opensearch index"""
     response = ops.reindex()
-    print(f'Index with {len(response["items"])} documents created '
-          f'in {response["took"]} milliseconds')
+    print(
+        f"Index with {len(response['items'])} documents created "
+        f"in {response['took']} milliseconds"
+    )
